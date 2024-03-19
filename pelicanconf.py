5992812d62f8bf222614ca38e3d4be567f02b284
Lennart Landsmeer <lennart@landsmeer.email>
2024-03-19 14:56:03 +0100

    publish

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,9 @@
 AUTHOR = 'Lennart Landsmeer'
-SITENAME = 'Lennart Landsmeer'
-SITEURL = "https://blog.llandsmeer.com"
+SITENAME = 'The Weekend Writeup'
+SITESUBTITLE = 'Lennart Landsmeer\'s Personal Blog'
+SITEURL = ''
+
+THEME='./theme'
 
 PATH = "content"
 
@@ -13,8 +16,10 @@ FEED_ATOM = ('atom.xml')
 CATEGORY_FEED_ATOM = None
 FEED_DOMAIN = SITEURL
 TRANSLATION_FEED = None
+SUMMARY_MAX_LENGTH = 100
 
 DEFAULT_CATEGORY = 'tech'
+DISPLAY_CATEGORIES_ON_MENU = False
 WITH_FUTURE_DATES = False
 
 SLUGIFY_SOURCE = 'basename'
@@ -29,19 +34,21 @@ ARTICLE_URL = '{category}/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
 ARTICLE_SAVE_AS = '{category}/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
 
 # Blogroll
-LINKS = (
-    ("Github", "https://github.com/llandsmeer"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
+LINKS = []
+# (
+#     ("Github", "https://github.com/llandsmeer"),
+#     ("Python.org", "https://www.python.org/"),
+#     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+#     ("You can modify those links in your config file", "#"),
+# )
 
 # Social widget
-SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
-)
-
+SOCIAL = []
+# (
+#     ("You can add links in your config file", "#"),
+#     ("Another social link", "#"),
+# )
+# 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
